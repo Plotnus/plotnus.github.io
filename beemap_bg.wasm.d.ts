e6ead2f24e2f4a7a765769bdb9f2a18c271ebff4
Jonathan Plotner <plotnus@gmail.com>
2021-07-12 07:36:33 -0700

    try fix post

--- beemap_bg.wasm.d.ts
+++ beemap_bg.wasm.d.ts
@@ -2,5 +2,12 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export function main(a: number, b: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__Fn__A_B_C___Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h24b17cfd51c31026(a: number, b: number, c: number, d: number, e: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__ha48ed26666c48c4e(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hbff7fea09bb91113(a: number, b: number, c: number): void;
 export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h4d35d51c9fe0e265(a: number, b: number, c: number, d: number): void;
 export function __wbindgen_start(): void;
